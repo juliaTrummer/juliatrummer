7b66bfc841326a4540465ad50d015994d0e61c33
Julia Trummer <juliatrummer@Julias-MacBook-Pro.local>
2023-03-08 12:15:44 +0100

    split php contacts into own file

--- index.php
+++ index.php
@@ -20,28 +20,8 @@
       <title>Julia Trummer</title>
 </head>
 <body>
-    <?php include 'footer.php';?>
-    <?php
-    $email= $_POST['email'];
-    $name= $_POST['user'];
-    $password = $_POST['password'];
-    if(isset($_POST['submit'])) {
-      // Check if name has been entered
-      if(empty($_POST['user'])) {
-        $errName= 'Please enter your user name';
-      }
-      // Check if email has been entered and is valid
-      else if(empty($_POST['email'])) {
-        $errEmail = 'Please enter a valid email address';
-      }
-      // check if a password has been entered and if it is a valid password
-      else if(empty($_POST['password']) || (preg_match("/^.*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$/", $_POST["password"]) === 0)) {
-        $errPass = '<p class="errText">Password must be at least 8 characters and must contain at least one lower case letter, one upper case letter and one digit</p>';
-      } else {
-        echo "The form has been submitted";
-      }
-    }
-    ?>
+    <?php include './inc/contact.php';?>
+
 
   <header id="headerCarousel " class="container-fluid p-0 m-0">
           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
@@ -104,40 +84,38 @@
       </div>
       </nav>
   </section>
-
     <section>
-
-  <div class="container">
-       <h1>CONTACT</h1>
-        <form role="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-          <div class="form-group row">
-            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
-            <div class="col-sm-10">
-              <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
-              <?php echo $errEmail; ?>
-            </div>
+        <div class="container">
+           <h1>CONTACT</h1>
+            <form role="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+              <div class="form-group row">
+                <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
+                  <?php echo $errEmail; ?>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputUser" class="col-sm-2 col-form-label">User Name</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputUser" name="user" placeholder="Username">
+                  <?php echo $errName; ?>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Password">
+                  <?php echo $errPass; ?>
+                </div>
+              </div>
+              <div class="form-group row">
+                <div class="offset-sm-2 col-sm-10">
+                  <input type="submit" value="Sign in" name="submit" class="btn btn-primary"/>
+                </div>
+              </div>
+            </form>
           </div>
-          <div class="form-group row">
-            <label for="inputUser" class="col-sm-2 col-form-label">User Name</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="inputUser" name="user" placeholder="Username">
-              <?php echo $errName; ?>
-            </div>
-          </div>
-          <div class="form-group row">
-            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Password">
-              <?php echo $errPass; ?>
-            </div>
-          </div>
-          <div class="form-group row">
-            <div class="offset-sm-2 col-sm-10">
-              <input type="submit" value="Sign in" name="submit" class="btn btn-primary"/>
-            </div>
-          </div>
-        </form>
-      </div>
   </section>
 
   <!--- Import last (performance) --->
